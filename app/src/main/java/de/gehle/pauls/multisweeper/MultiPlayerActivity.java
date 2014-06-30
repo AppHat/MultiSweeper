88f9e921d55607dfacb2fca2f844cd65ba8928c6
Andreas Gehle <andigehle@gmail.com>
2014-06-30 20:24:58 +0200

    Added waiting room

--- app/src/main/java/de/gehle/pauls/multisweeper/MultiPlayerActivity.java
+++ app/src/main/java/de/gehle/pauls/multisweeper/MultiPlayerActivity.java
@@ -10,9 +10,12 @@ import android.view.View;
 import android.view.WindowManager;
 
 import com.google.android.gms.games.Games;
+import com.google.android.gms.games.GamesActivityResultCodes;
+import com.google.android.gms.games.GamesStatusCodes;
 import com.google.android.gms.games.multiplayer.Invitation;
 import com.google.android.gms.games.multiplayer.Multiplayer;
 import com.google.android.gms.games.multiplayer.OnInvitationReceivedListener;
+import com.google.android.gms.games.multiplayer.Participant;
 import com.google.android.gms.games.multiplayer.realtime.RealTimeMessage;
 import com.google.android.gms.games.multiplayer.realtime.RealTimeMessageReceivedListener;
 import com.google.android.gms.games.multiplayer.realtime.Room;
@@ -31,6 +34,17 @@ public class MultiPlayerActivity extends BaseGameActivity implements OnInvitatio
      * can be any number as long as it's unique
      */
     final static int RC_SELECT_PLAYERS = 10000;
+
+    /**
+     * Arbitrary request code for the waiting room UI.
+     * This can be any integer that's unique in your Activity.
+     */
+    final static int RC_WAITING_ROOM = 10002;
+
+    final static int MIN_OTHER_PLAYERS = 1;
+    final static int MAX_OTHER_PLAYERS = 2;
+
+    private String mRoomId;
 
     private static final String TAG = "Multiplayer";
 
@@ -63,7 +77,7 @@ public class MultiPlayerActivity extends BaseGameActivity implements OnInvitatio
     public void startQuickGame(View view) {
         // auto-match criteria to invite one random automatch opponent.
         // You can also specify more opponents (up to 3).
-        Bundle am = RoomConfig.createAutoMatchCriteria(1, 1, 0);
+        Bundle am = RoomConfig.createAutoMatchCriteria(MIN_OTHER_PLAYERS, MAX_OTHER_PLAYERS, 0);
 
         // build the room config:
         RoomConfig.Builder roomConfigBuilder = makeBasicRoomConfigBuilder();
@@ -82,7 +96,7 @@ public class MultiPlayerActivity extends BaseGameActivity implements OnInvitatio
     public void invitePlayer(View view) {
         // launch the player selection screen
         // minimum: 1 other player; maximum: 3 other players
-        Intent intent = Games.RealTimeMultiplayer.getSelectOpponentsIntent(getApiClient(), 1, 3);
+        Intent intent = Games.RealTimeMultiplayer.getSelectOpponentsIntent(getApiClient(), MIN_OTHER_PLAYERS, MAX_OTHER_PLAYERS);
         startActivityForResult(intent, RC_SELECT_PLAYERS);
     }
 
@@ -108,7 +122,24 @@ public class MultiPlayerActivity extends BaseGameActivity implements OnInvitatio
 
     @Override
     public void onActivityResult(int request, int response, Intent data) {
-        if (request == RC_SELECT_PLAYERS) {
+        if (request == RC_WAITING_ROOM) {
+            if (response == Activity.RESULT_OK) {
+                // (start game)
+            } else if (response == Activity.RESULT_CANCELED) {
+                // Waiting room was dismissed with the back button. The meaning of this
+                // action is up to the game. You may choose to leave the room and cancel the
+                // match, or do something else like minimize the waiting room and
+                // continue to connect in the background.
+
+                // in this example, we take the simple approach and just leave the room:
+                Games.RealTimeMultiplayer.leave(getApiClient(), this, mRoomId);
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            } else if (response == GamesActivityResultCodes.RESULT_LEFT_ROOM) {
+                // player wants to leave the room.
+                Games.RealTimeMultiplayer.leave(getApiClient(), this, mRoomId);
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        } else if (request == RC_SELECT_PLAYERS) {
             if (response != Activity.RESULT_OK) {
                 // user canceled
                 return;
@@ -170,23 +201,54 @@ public class MultiPlayerActivity extends BaseGameActivity implements OnInvitatio
     //=================================================
 
     @Override
-    public void onRoomCreated(int i, Room room) {
+    public void onRoomCreated(int statusCode, Room room) {
+        if (statusCode != GamesStatusCodes.STATUS_OK) {
+            // let screen go to sleep
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+            // show error message, return to main screen.
+            return;
+        }
+
+        mRoomId = room.getRoomId();
+
+        // get waiting room intent
+        Intent i = Games.RealTimeMultiplayer.getWaitingRoomIntent(getApiClient(), room, Integer.MAX_VALUE);
+        startActivityForResult(i, RC_WAITING_ROOM);
     }
 
     @Override
-    public void onJoinedRoom(int i, Room room) {
+    public void onJoinedRoom(int statusCode, Room room) {
+        if (statusCode != GamesStatusCodes.STATUS_OK) {
+            // let screen go to sleep
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+            // show error message, return to main screen.
+            return;
+        }
+
+        mRoomId = room.getRoomId();
+
+        // get waiting room intent
+        Intent i = Games.RealTimeMultiplayer.getWaitingRoomIntent(getApiClient(), room, Integer.MAX_VALUE);
+        startActivityForResult(i, RC_WAITING_ROOM);
     }
 
     @Override
     public void onLeftRoom(int i, String s) {
-
+        mRoomId = null;
     }
 
     @Override
-    public void onRoomConnected(int i, Room room) {
+    public void onRoomConnected(int statusCode, Room room) {
+        if (statusCode != GamesStatusCodes.STATUS_OK) {
+            // let screen go to sleep
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+            // show error message, return to main screen.
+        }
+
+        mRoomId = room.getRoomId();
     }
 
     //=================================================
@@ -197,6 +259,31 @@ public class MultiPlayerActivity extends BaseGameActivity implements OnInvitatio
     }
 
     //=================================================
+
+
+    // are we already playing?
+    boolean mPlaying = false;
+
+    // at least 2 players required for our game
+    final static int MIN_PLAYERS = 2;
+
+    // returns whether there are enough players to start the game
+    private boolean shouldStartGame(Room room) {
+        int connectedPlayers = 0;
+        for (Participant p : room.getParticipants()) {
+            if (p.isConnectedToRoom()) ++connectedPlayers;
+        }
+        return connectedPlayers >= MIN_PLAYERS;
+    }
+
+    // Returns whether the room is in a state where the game should be canceled.
+    private boolean shouldCancelGame(Room room) {
+        // TODO: Your game-specific cancellation logic here. For example, you might decide to
+        // cancel the game if enough people have declined the invitation or left the room.
+        // You can check a participant's status with Participant.getStatus().
+        // (Also, your UI should have a Cancel button that cancels the game too)
+        return false;
+    }
 
     @Override
     public void onRoomConnecting(Room room) {
@@ -214,8 +301,12 @@ public class MultiPlayerActivity extends BaseGameActivity implements OnInvitatio
     }
 
     @Override
-    public void onPeerDeclined(Room room, List<String> strings) {
-
+    public void onPeerDeclined(Room room, List<String> peers) {
+        // peer declined invitation -- see if game should be canceled
+        if (!mPlaying && shouldCancelGame(room)) {
+            Games.RealTimeMultiplayer.leave(getApiClient(), null, mRoomId);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
     }
 
     @Override
@@ -224,28 +315,44 @@ public class MultiPlayerActivity extends BaseGameActivity implements OnInvitatio
     }
 
     @Override
-    public void onPeerLeft(Room room, List<String> strings) {
-
+    public void onPeerLeft(Room room, List<String> peers) {
+        // peer left -- see if game should be canceled
+        if (!mPlaying && shouldCancelGame(room)) {
+            Games.RealTimeMultiplayer.leave(getApiClient(), null, mRoomId);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
     }
 
     @Override
     public void onConnectedToRoom(Room room) {
-
+        mRoomId = room.getRoomId();
     }
 
     @Override
     public void onDisconnectedFromRoom(Room room) {
-
+        mRoomId = null;
     }
 
     @Override
-    public void onPeersConnected(Room room, List<String> strings) {
-
+    public void onPeersConnected(Room room, List<String> peers) {
+        if (mPlaying) {
+            // add new player to an ongoing game
+        } else if (shouldStartGame(room)) {
+            // start game!
+        }
     }
 
     @Override
-    public void onPeersDisconnected(Room room, List<String> strings) {
-
+    public void onPeersDisconnected(Room room, List<String> peers) {
+        if (mPlaying) {
+            // do game-specific handling of this -- remove player's avatar
+            // from the screen, etc. If not enough players are left for
+            // the game to go on, end the game and leave the room.
+        } else if (shouldCancelGame(room)) {
+            // cancel the game
+            Games.RealTimeMultiplayer.leave(getApiClient(), null, mRoomId);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
     }
 
     @Override
